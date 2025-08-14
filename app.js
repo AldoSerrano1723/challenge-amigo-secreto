2ad7806dd3a09794908fd7577796149f8fea69a0
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-08-14 11:30:51 -0600

    feat: agrega funcion agregar amigo
    incluye tomar el valor del input y guardar ese valor en la listaDeAmigos

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// variables
+
+// variables:
 let listaDeAmigos = [];
+// --------------------------------------------------
+// funciones:
+// funcion para el boton añadir - añade un amigo a la lista de listaDeAmigos
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(nombreAmigo);
+    console.log(listaDeAmigos);
+    listaDeAmigos.push(nombreAmigo);
+    console.log(listaDeAmigos);
+}
+
