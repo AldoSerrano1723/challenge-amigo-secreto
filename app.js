0c52db18093db876fb69ee568aa139fef8e63e09
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-08-15 12:01:40 -0600

    feat: actualiza funcion mostrarListaDeAmigos

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ let listaDeAmigos = [];
 // funciones:
 // funcion para el boton añadir - añade un amigo a la lista de listaDeAmigos
 function agregarAmigo() {
+    // aqui obtengo el contenido del input
     let nombreAmigo = document.getElementById("amigo").value;
     console.log(typeof(nombreAmigo));
+    
     // verifica si esta vacio la variable nombreAmigo o no
     if (nombreAmigo === "") {
         alert("Por favor, inserte un nombre");
@@ -15,10 +17,28 @@ function agregarAmigo() {
         listaDeAmigos.push(nombreAmigo);
         console.log(listaDeAmigos);
     }
-    document.getElementById("amigo").value = "";
-}
-function actualizaListaAmigos() {
     
+    // limpio el input
+    document.getElementById("amigo").value = "";
+    
+    // aqui muestra la lista de los amigos que vayas agregando
+    mostrarListaDeAmigos();
+}
+
+// funcion para mostrar en pantalla a los amigos que agregas a tu lista 
+function mostrarListaDeAmigos() {
+    // aqui conecto la lista de HTML con java por medio del ID de HTML
+    let listaDeAmigosHtml = document.getElementById("listaAmigos");
+
+    // aqui se limpia la lista para no mostrar duplicados
+    listaDeAmigosHtml.innerHTML = "";
+
+    // aqui recorre el lista y se crea un nuevo elemento de la lista
+    listaDeAmigos.forEach(amigo => {
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = amigo;
+        listaDeAmigosHtml.appendChild(nuevoElemento);
+    });
 }
 
 // ---------------------------------------------------
