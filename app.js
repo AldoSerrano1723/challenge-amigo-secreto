3d5906e1aa104bc6c238af138728ff88043d6864
AldoSerrano1723 <150762595+AldoSerrano1723@users.noreply.github.com>
2025-08-19 10:38:24 -0600

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+console.log("Bienvenido");
 // variables:
 let listaDeAmigos = [];
 // --------------------------------------------------
